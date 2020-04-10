6806a8c7c28d3272d1715ba5025bfeaa52a43b6d
Yuri Ramocan <yuri.ramocan@detroitlabs.com>
2020-04-09 22:48:05 -0400

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Yuri Ramocan on 4/9/20.
+//  Copyright © 2020 Yuri Ramocan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
